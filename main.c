c71452994c624d18a5ff954fdb159c68ba7b40cf
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-12-06 14:05:54 +0100

    added 0 test

--- main.c
+++ main.c
@@ -6,8 +6,14 @@ int main(void)
     int a;
     int b;
 
+    // Hex test
     a = printf(" %x %x %x \n", 0, 15, 255);
     b = ft_printf(" %x %x %x \n", 0, 15, 255);
+    printf("printf = %d, ft_printf = %d\n\n", a, b);
+
+    // Pointer NULL test (matches TEST(9, print(" %p %p ", 0, 0));)
+    a = printf(" %p %p \n", (void *)0, (void *)0);
+    b = ft_printf(" %p %p \n", (void *)0, (void *)0);
     printf("printf = %d, ft_printf = %d\n", a, b);
     return (0);
 }
