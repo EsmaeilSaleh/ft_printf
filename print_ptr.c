ead3f3401dca1794c196687358577df692adf8ad
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-12-06 14:07:52 +0100

    fix ptr

--- print_ptr.c
+++ print_ptr.c
@@ -1,10 +1,12 @@
 #include "ft_printf.h"
 
-int	print_ptr(void *p)
+int print_ptr(void *p)
 {
-	unsigned long	addr;
-	int				count;
+	unsigned long addr;
+	int count;
 
+	if (!p)
+		return (write(1, "(nil)", 5));
 	addr = (unsigned long)p;
 	count = write(1, "0x", 2);
 	count += print_hex(addr, 0);
